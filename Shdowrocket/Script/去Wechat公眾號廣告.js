c23ff5b6723ca73fc1487aa88532274bf03a086e
kapionhau <50528435+kapionhau@users.noreply.github.com>
2021-12-12 19:24:10 +0800

    Update 去Wechat公眾號廣告.js

--- Shdowrocket/Script/去Wechat公眾號廣告.js
+++ Shdowrocket/Script/去Wechat公眾號廣告.js
@@ -3,13 +3,13 @@ Wechat 去除公眾號底部文章
 ***************************
 QuantumultX:
 [rewrite_local]
-^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/kapionhau/Fly/Script/main/去Wechat公眾號廣告.js
+^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/kapionhau/Fly/main/Shdowrocket/Script/去Wechat公眾號廣告.js
 [mitm]
 hostname = mp.weixin.qq.com
 ***************************
 Surge4 or Loon:
 [Script]
-http-response ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/kapionhau/Fly/Script/main/去Wechat公眾號廣告.js
+http-response ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/kapionhau/Fly/main/Shdowrocket/Script/去Wechat公眾號廣告.js
 [MITM]
 hostname = mp.weixin.qq.com
 **************************/
