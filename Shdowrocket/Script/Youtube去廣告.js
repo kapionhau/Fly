89e05298547c6d70ca1ad4d4710c8787dea8fa8b
kapionhau <50528435+kapionhau@users.noreply.github.com>
2021-12-12 20:32:09 +0800

    Update Youtube去廣告.js

--- Shdowrocket/Script/Youtube去廣告.js
+++ Shdowrocket/Script/Youtube去廣告.js
@@ -1,7 +1,37 @@
-/* 更新時間:2021-12-12
-   來源https://github.com/Choler/Surge/tree/master/Script
-   好像太新版本出現中間廣告會無限重播
-*/
+/* 
+更新時間:2021-12-12
+Youtube去廣告
+來源https://github.com/Choler/Surge/tree/master/Script
+如果是YouTube Premium就不要開啟該腳本
+好像太高版本出現中間廣告會無限重播
+源地址未改
+***************************
+QuantumultX:
+
+[rewrite_local]
+^https?:\/\/.+?\.googlevideo\.com\/.+&(oad|ctier)=(?!A) url script-response-body https://choler.github.io/Surge/Script/YouTube.js,script-update-interval=0
+
+[mitm]
+hostname = *.googlevideo.com
+
+***************************
+Surge4 or Loon:
+
+[Script]
+http-response ^https?:\/\/.+?\.googlevideo\.com\/.+&(oad|ctier)=(?!A) script-path=https://choler.github.io/Surge/Script/YouTube.js,script-update-interval=0,enable = true
+
+[MITM]
+hostname = *.googlevideo.com
+***************************
+Shadowrocket:
+
+[Script]
+YouTube去廣告 = type=http-request,pattern=^https?:\/\/.+?\.googlevideo\.com\/.+&(oad|ctier)=(?!A),script-path=https://choler.github.io/Surge/Script/YouTube.js,script-update-interval=0,enable = true
+[MITM]
+hostname = *.googlevideo.com
+
+***************************/
+
 
 if ($request.url.indexOf("&oad") != -1) {
   $done({ response: {body: ""} });
